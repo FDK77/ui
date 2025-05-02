a73e9f68a10c9cf5ba6b2d9a4de36730e34f1d95
Daniil <daniilkarp2003@mail.ru>
2025-05-03 00:23:43 +0400

    delete string mod

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 
@@ -13,10 +12,8 @@ async function deferRender() {
 }
 deferRender().then(() => {
   createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
   )
 })
