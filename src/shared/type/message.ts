aa223ef763b1c77ec1209dd53eb3d9e1e948c232
Daniil <daniilkarp2003@mail.ru>
2025-05-03 00:55:33 +0400

    add type user and message

---
+++ src/shared/type/message.ts
@@ -0,0 +1,9 @@
+import { User } from './user'
+
+export interface Message {
+  id: number
+  text: string
+  summary: null | string
+  timestamp: string
+  sender: User
+}
---
+++ src/shared/type/user.ts
@@ -0,0 +1,7 @@
+export interface User {
+  userId: number
+  username: string
+  phoneNumber: number | null
+  displayName: string
+  avatarPath: string | null
+}
